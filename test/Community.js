172404e84faab944ca018e2dea66e42bb59dc4d6
vishnu.krishnan@ults.in <vishnu.krishnan>
2021-04-18 18:00:29 +0530

    removed test folder from src

--- test/Community.js
+++ test/Community.js
@@ -9,65 +9,65 @@ require('chai')
   .should()
 
 contract('CommunityTour', ([deployer,Investor,  Depty]) => {
-  let CommunityTour
-  let result;
+    let CommunityTour
+    let result;
 
-  before(async () => {
-     
-    CommunityTour = await contracts.deployed()
-    await CommunityTour.become_Investor(Investor)
-    await CommunityTour.become_Depty(Depty)
+    before(async () => {
+      
+      CommunityTour = await contracts.deployed()
+      await CommunityTour.become_Investor(Investor)
+      await CommunityTour.become_Depty(Depty)
+    })
+
+    describe('transfer', async () => {
+      
+      it('Money transfer', async () => {
+        // Track the Investor balance before purchase
+        let oldInvestorBalance
+        oldInvestorBalance = await web3.eth.getBalance(Investor)
+        oldInvestorBalance = new web3.utils.BN(oldInvestorBalance)
+
+        result = await CommunityTour.Transfer(Depty, { from:Investor, value: web3.utils.toWei('1', 'Ether') })
+        let newInvestorBalance = await web3.eth.getBalance(Depty)
+
+        assert.notEqual(newInvestorBalance.toString(),oldInvestorBalance.toString())
+
+    })
   })
 
-  describe('transfer', async () => {
-    
-    it('Money transfer', async () => {
-      // Track the Investor balance before purchase
-      let oldInvestorBalance
-      oldInvestorBalance = await web3.eth.getBalance(Investor)
-      oldInvestorBalance = new web3.utils.BN(oldInvestorBalance)
+    describe('blance', async () => {
+      
+      it('balance of depty to payed', async () => {
+        let tipAmount = web3.utils.toWei('1', 'Ether')
 
-      result = await CommunityTour.Transfer(Depty, { from:Investor, value: web3.utils.toWei('1', 'Ether') })
-      let newInvestorBalance = await web3.eth.getBalance(Depty)
+        // result = await CommunityTour.Transfer(Investor, { from:Depty, value: web3.utils.toWei('1', 'Ether') })
+        let balance= await CommunityTour.getDeptAmount(Depty)
 
-      assert.notEqual(newInvestorBalance.toString(),oldInvestorBalance.toString())
+        assert.equal(balance,tipAmount)
 
+    })
+      it('balance of Investment ', async () => {
+        let tipAmount = web3.utils.toWei('2', 'Ether')
+
+        result = await CommunityTour.Transfer(Depty, { from:Investor, value: web3.utils.toWei('1', 'Ether') })
+        let balance= await CommunityTour.getTotalAmountInvested(Investor)
+        
+        assert.equal(balance,tipAmount)
+
+    })
   })
-})
 
-  describe('blance', async () => {
-    
-    it('balance of depty to payed', async () => {
+  describe('repayment ', async () => {
+      
+    it('balance ', async () => {
       let tipAmount = web3.utils.toWei('1', 'Ether')
 
-      // result = await CommunityTour.Transfer(Investor, { from:Depty, value: web3.utils.toWei('1', 'Ether') })
-      let balance= await CommunityTour.getDeptAmount(Depty)
+      result = await CommunityTour.repay(Investor, { from:Depty, value: web3.utils.toWei('1', 'Ether') })
+      const balance=  await CommunityTour.getDeptAmount(Depty)
 
-      assert.equal(balance,tipAmount)
+      assert.equal(balance , tipAmount)
 
+    })
   })
-  it('balance of Investment ', async () => {
-    let tipAmount = web3.utils.toWei('2', 'Ether')
-
-    result = await CommunityTour.Transfer(Depty, { from:Investor, value: web3.utils.toWei('1', 'Ether') })
-    let balance= await CommunityTour.getTotalAmountInvested(Investor)
-     
-    assert.equal(balance,tipAmount)
-
-})
-})
-
-describe('repayment ', async () => {
-    
-  it('balance ', async () => {
-    let tipAmount = web3.utils.toWei('1', 'Ether')
-
-    result = await CommunityTour.repay(Investor, { from:Depty, value: web3.utils.toWei('1', 'Ether') })
-    const balance=  await CommunityTour.getDeptAmount(Depty)
-
-    assert.equal(balance , tipAmount)
-
-})
-})
 
 })
